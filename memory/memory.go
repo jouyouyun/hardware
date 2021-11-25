2cd7fc1abe384c48b7ebeb93549a560138eec0f5
baiyuxiong <baiyuxiong@uniontech.com>
2021-11-25 22:58:37 +0800

    chore: Read memory info (Manufacturer,Size,SerialNumber ... ) from dmi table

--- memory/memory.go
+++ memory/memory.go
@@ -1,54 +1,74 @@
 package memory
 
 import (
-	"strconv"
-	"strings"
-
-	"github.com/jouyouyun/hardware/utils"
+	"github.com/yumaojun03/dmidecode"
 )
 
-// Memory store memory info
-type Memory struct {
-	Name         string // TODO(jouyouyun): implement
-	Manufacturer string // TODO(jouyouyun): implement
-	Capacity     int64  // kb
-}
-
-// MemoryList memory list
 type MemoryList []*Memory
-
-const (
-	memKeyTotal = "MemTotal"
-	memKeyDelim = ":"
-	memFilename = "/proc/meminfo"
-)
 
 var (
 	_memList MemoryList
 )
 
+type Memory struct {
+	Manufacturer               string
+	Capacity                   uint64
+	PhysicalMemoryArrayHandle  uint16
+	ErrorInformationHandle     uint16
+	TotalWidth                 uint16
+	DataWidth                  uint16
+	FormFactor                 string
+	DeviceSet                  byte
+	DeviceLocator              string
+	BankLocator                string
+	Type                       string
+	TypeDetail                 string
+	Speed                      uint16
+	SerialNumber               string
+	AssetTag                   string
+	PartNumber                 string
+	Attributes                 byte
+	ExtendedSize               uint32
+	ConfiguredMemoryClockSpeed uint16
+	MinimumVoltage             uint16
+	MaximumVoltage             uint16
+	ConfiguredVoltage          uint16
+}
+
 func GetMemoryList() (MemoryList, error) {
 	if len(_memList) == 0 {
-		mem, err := getMemory(memFilename)
+		dmi, err := dmidecode.New()
 		if err != nil {
 			return nil, err
 		}
-		_memList = MemoryList{mem}
+
+		devices, err := dmi.MemoryDevice()
+		for _, device := range devices {
+			_memList = append(_memList, &Memory{
+				Manufacturer:               device.Manufacturer,
+				Capacity:                   uint64(device.Size) * 1024, // MB TO KB
+				PhysicalMemoryArrayHandle:  device.PhysicalMemoryArrayHandle,
+				ErrorInformationHandle:     device.ErrorInformationHandle,
+				TotalWidth:                 device.TotalWidth,
+				DataWidth:                  device.DataWidth,
+				FormFactor:                 device.FormFactor.String(),
+				DeviceSet:                  device.DeviceSet,
+				DeviceLocator:              device.DeviceLocator,
+				BankLocator:                device.BankLocator,
+				Type:                       device.Type.String(),
+				TypeDetail:                 device.TypeDetail.String(),
+				Speed:                      device.Speed,
+				SerialNumber:               device.SerialNumber,
+				AssetTag:                   device.AssetTag,
+				PartNumber:                 device.PartNumber,
+				Attributes:                 device.Attributes,
+				ExtendedSize:               device.ExtendedSize,
+				ConfiguredMemoryClockSpeed: device.ConfiguredVoltage,
+				MinimumVoltage:             device.ConfiguredVoltage,
+				MaximumVoltage:             device.ConfiguredVoltage,
+				ConfiguredVoltage:          device.ConfiguredVoltage,
+			})
+		}
 	}
 	return _memList, nil
-}
-
-func getMemory(filename string) (*Memory, error) {
-	var keySet = map[string]string{memKeyTotal: ""}
-	err := utils.ProcGetByKey(filename, memKeyDelim,
-		keySet, false)
-	if err != nil {
-		return nil, err
-	}
-	items := strings.SplitN(keySet[memKeyTotal], " ", 2)
-	total, err := strconv.ParseInt(items[0], 10, 64)
-	if err != nil {
-		return nil, err
-	}
-	return &Memory{Capacity: total}, nil
 }
